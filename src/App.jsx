a7196f4637ac5a8cba9fe524b874351286f5f959
japneet <japneetsaini084@gmail.com>
2023-06-23 20:03:04 +0530

    created layout and states

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import "./App.css"
 
-function myButton(){
-
-  let [count, setCount] = useState();
-  function handleClick(){
-    setCount(count++);
-    console.log("Clicked");
-  }
-  return(
-    <button onClick = {handleClick}>
-      Clicked {count} times
-    </button>
-  )
-}
-
-function Square(){
-  const [value, setValue ] = useState(null);
-  function handleClick(){
-    alert('clicked');
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    // alert('clicked')
     setValue('X')
   }
-  return (
-    <button className="square" onClick={handleCluck}>{value}</button>
-  );
+  return <button onClick={handleClick} className='square'>{value}</button>
 }
 
-function App() {
 
+function App() {
   return (
     <div className='app'>
       <div className='board-row'>
-        <Square value='1'/>
-        <Square value='3'/>
-        <Square value='2'/>
+        <Square value='1' />
+        <Square value='2' />
+        <Square value='3' />
       </div>
-      
       <div className='board-row'>
-        <Square value='4'/>
-        <Square value='5'/>
-        <Square value='6'/>
+        <Square value='4' />
+        <Square value='5' />
+        <Square value='6' />
       </div>
-
       <div className='board-row'>
-        <Square value='7'/>
-        <Square value='8'/>
-        <Square value='9'/>
+        <Square value='7' />
+        <Square value='8' />
+        <Square value='9' />
       </div>
 
     </div>
