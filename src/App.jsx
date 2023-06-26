aed6465bfe2f0f96d6d334e0d01bd0ca71c073a5
japneet <japneetsaini084@gmail.com>
2023-06-26 19:53:33 +0530

    created 2 signs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
 import React, { useState } from 'react';
 import "./App.css"
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square(props) {
   function handleClick() {
     // alert('clicked')
-    setValue('X')
+    
   }
-  return <button onClick={handleClick} className='square'>{value}</button>
+  return <button onClick={props.onSquareClick} className='square'>{props.value}</button>
 }
 
+function Board(){
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setIsNext] = useState(true);
+  console.log(squares);
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    if(xIsNext){
+      nextSquares[i] = 'X';
+    }else{
+      nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setIsNext(!xIsNext)
+  }
+
+  return(
+    <div className='app'>
+    <div className='board-row'>
+        <Square value={squares[0]} onSquareClick={() =>handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() =>handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() =>handleClick(2)}/>
+      </div>
+      <div className='board-row'>
+        <Square value={squares[3]} onSquareClick={() =>handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() =>handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() =>handleClick(5)}/>
+      </div>
+      <div className='board-row'>
+        <Square value={squares[6]} onSquareClick={() =>handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() =>handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() =>handleClick(8)}/>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  console.log(squares);
   return (
     <div className='app'>
-      <div className='board-row'>
-        <Square value='1' />
-        <Square value='2' />
-        <Square value='3' />
-      </div>
-      <div className='board-row'>
-        <Square value='4' />
-        <Square value='5' />
-        <Square value='6' />
-      </div>
-      <div className='board-row'>
-        <Square value='7' />
-        <Square value='8' />
-        <Square value='9' />
-      </div>
-
+      <Board/>
     </div>
   )
 }
